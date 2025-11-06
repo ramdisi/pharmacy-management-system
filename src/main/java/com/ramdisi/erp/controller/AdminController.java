1fddb9bd50b1ec51edcd8420145046914585f4d4
ramdisi <ramdisiabey@gmail.com>
2025-11-06 23:53:59 +0530

    add:admin UI and boilerplate code

---
+++ src/main/java/com/ramdisi/erp/controller/AdminController.java
@@ -0,0 +1,51 @@
+package com.ramdisi.erp.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+public class AdminController {
+
+    @FXML
+    private Label lbl_numberOfNotifications;
+
+    @FXML
+    private Label lbl_time;
+
+    @FXML
+    void btn_onAction_HRManagement(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btn_onAction_salesStatics(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btn_onAction_stockManagement(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btn_onAction_supplierManagement(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btn_onAction_systemUserManagement(ActionEvent event) {
+
+    }
+
+    @FXML
+    void img_onAction_logout(MouseEvent event) {
+
+    }
+
+    @FXML
+    void img_onAction_notifications(MouseEvent event) {
+
+    }
+
+}
