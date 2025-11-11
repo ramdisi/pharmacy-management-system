9ea364cd8db6bf7b0d98fa2c6c70c969a01fa451
ramdisi <ramdisiabey@gmail.com>
2025-11-11 12:24:11 +0530

    add:checkout UI and boilerplate code

---
+++ src/main/java/com/ramdisi/erp/controller/Checkout.java
@@ -0,0 +1,91 @@
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
+
+public class PleaseProvideControllerClassName {
+
+    @FXML
+    private JFXButton btn_addAnotherItemToBill;
+
+    @FXML
+    private JFXButton btn_cancelOrder;
+
+    @FXML
+    private JFXButton btn_newOrder;
+
+    @FXML
+    private JFXButton btn_proceedPayment;
+
+    @FXML
+    private JFXButton btn_removeSelectedFromCart;
+
+    @FXML
+    private TableColumn<?, ?> col_batchId;
+
+    @FXML
+    private TableColumn<?, ?> col_name;
+
+    @FXML
+    private TableColumn<?, ?> col_pricePerItem;
+
+    @FXML
+    private TableColumn<?, ?> col_quantity;
+
+    @FXML
+    private TableColumn<?, ?> col_total;
+
+    @FXML
+    private Label lbl_amountAfterDiscount;
+
+    @FXML
+    private Label lbl_cashierName;
+
+    @FXML
+    private Label lbl_time;
+
+    @FXML
+    private TableView<?> tblView_bill;
+
+    @FXML
+    private JFXTextField txt_payment;
+
+    @FXML
+    private JFXTextField txt_telNo;
+
+    @FXML
+    void btn_onAction_addAnotherItemToBill(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btn_onAction_cancelOrder(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btn_onAction_newOrder(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btn_onAction_proceedPayment(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btn_onAction_removeSelectedFromCart(ActionEvent event) {
+
+    }
+
+    @FXML
+    void img_onClick_logout(MouseEvent event) {
+
+    }
+
+}
+
